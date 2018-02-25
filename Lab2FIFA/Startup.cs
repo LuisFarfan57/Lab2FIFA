2c7d939fcbffb0fdcac912517c32ee60c07be74c
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2018-02-25 10:43:53 -0600

    Add files via upload
    proyecto

---
+++ Lab2FIFA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab2FIFA.Startup))]
+namespace Lab2FIFA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
